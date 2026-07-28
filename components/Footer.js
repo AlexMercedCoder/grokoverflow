77e71ac3fd38a2791237981a0a85b003bce43c43
Alex Merced <alex@alexmerced.dev>
2026-07-28 13:34:16 -0400

    add: footer disclaimer - views belong solely to Alex Merced, not any organization or employer

--- components/Footer.js
+++ components/Footer.js
@@ -7,6 +7,9 @@ function Footer(props) {
   return (
     <footer className={styles.footer}>
       copyright 2022 by Alex Merced of alexmercedcoder.dev
+      <p style={{fontSize:'0.8rem',marginTop:'0.25rem',opacity:0.7}}>
+        The views, thoughts, and opinions expressed on this site belong solely to Alex Merced and do not represent the views of any organization or employer.
+      </p>
     </footer>
   );
 }
